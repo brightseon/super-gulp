b3d4e08541912aacf8df71c1ec0037aa67516809
seon <seonu0823@gmail.com>
2019-08-15 14:24:38 +0900

    #1-0 Pug Task part One

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,13 @@
 import gulp from 'gulp';
+import gpug from 'gulp-pug';
+
+const routes = {
+    pug : {
+        src : 'src/*.pug',
+        dest : 'build'
+    }
+};
+
+export const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+
+export const dev = gulp.series([pug]);
