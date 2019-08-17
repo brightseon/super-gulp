3d304c95fc9ae959220ce630e29f4de57da601c9
seon <seonu0823@gmail.com>
2019-08-17 15:46:34 +0900

    #2-0 Dev Server

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import gpug from 'gulp-pug';
 import del from 'del';
+import ws from 'gulp-webserver';
 
 const routes = {
     pug : {
@@ -13,8 +14,12 @@ const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pu
 
 const clean = () => del(['build']);
 
+const webserver = () => gulp.src('build').pipe(ws({ livereloade : true, open : true }));
+
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([prepare, assets]);
+const postDev = gulp.series([webserver]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
