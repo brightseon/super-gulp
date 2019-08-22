78400d3c9d07e848b7c078e3f6c7bcba5a3d5896
seon <seonu0823@gmail.com>
2019-08-22 22:05:38 +0900

    #4-0 Gulp SASS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@ import gpug from 'gulp-pug';
 import del from 'del';
 import ws from 'gulp-webserver';
 import image from 'gulp-image';
+import sass from 'gulp-sass';
+
+sass.compiler = require('node-sass');
 
 const routes = {
     pug : {
@@ -13,6 +16,11 @@ const routes = {
     img : {
         src : 'src/img/*',
         dest : 'build/img'
+    },
+    scss : {
+        watch : 'src/scss/**/*.scss',
+        src : 'src/scss/style.scss',
+        dest : 'build/css'
     }
 };
 
@@ -24,14 +32,17 @@ const webserver = () => gulp.src('build').pipe(ws({ livereload : true, open : tr
 
 const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
+const styles = () => gulp.src(routes.scss.src).pipe(sass().on('error', sass.logError)).pipe(gulp.dest(routes.scss.dest))
+
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
     gulp.watch(routes.img.src, img);
+    gulp.watch(routes.scss.watch, styles);
 };
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const postDev = gulp.parallel([webserver, watch]);
 
